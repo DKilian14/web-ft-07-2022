1c0c7290a8476ccd5dc480a360fe3ba0e8e4fa1e
Dan Kilian <dankilian@yahoo.com>
2022-07-07 11:30:59 -0500

    added another repo to thurs, week 1 lecture notes

---
+++ lectures/week01/4-Thursday/homework/answers.py
@@ -0,0 +1,16 @@
+# Homework
+## Iterative Programming
+
+#### 1. Multiply Vectors
+
+# Given two lists of numbers of the same length, create a new list by multiplying the pairs of numbers in corresponding positions in the two lists. Example:
+
+# ```
+# [2, 4, 5] x [2, 3, 6] = [4, 12, 30]
+# ```
+
+list1 = [2,4,5]
+list2 = [2,3,6]
+
+for i in range(list1.length()):
+    print(list1[i])
