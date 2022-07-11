6ab01870f7bc21b8c722e0af1745d3c89955f6a2
Dan Kilian <dankilian@yahoo.com>
2022-07-11 15:26:19 -0500

    NOW i actually finished problem 2 of the extra problems

--- lectures/week02/1-Monday/homework/Extra_Problems_Answers.py
+++ lectures/week02/1-Monday/homework/Extra_Problems_Answers.py
@@ -1,7 +1,7 @@
-import math
 
 
-# 1. Write a function called power which accepts a base and an exponent.
+
+################### 1. Write a function called power which accepts a base and an exponent.
 # The function should return the power of the base to the exponent.
 
 
@@ -13,3 +13,25 @@ import math
 #     print(f"{base} to the power of {exponent} is {product}")
 
 # power(2,4)
+
+############### 2. Write a function factorial which accepts a number and returns
+# the factorial of that number.  A factorial is the product of an
+# interger and all the integers below it; eg. , factorial four( 4!) is
+# equal to 24, because 4 * 3 * 2 * 1 equals 24.  factorial zero (0!) is always 1.
+
+# -----------------------------ANSWERS -------------------------------------------
+# the_number = 7
+
+
+# def factorial(a_number):
+#     product = 1
+#     while a_number > 1: 
+#         product = product*a_number
+#         a_number -= 1
+        
+#     print(product)
+
+# factorial(the_number)
+
+############### 3. Write a function called recursiveRange which accepts a number and adds up all
+# the numbers from 0 to the number passed to the function
