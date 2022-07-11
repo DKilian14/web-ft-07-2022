1942ac4a76612bfd02aa1c10683b23649d24badd
Dan Kilian <dankilian@yahoo.com>
2022-07-11 15:16:03 -0500

    answered 1st and 2nd extra problems

---
+++ lectures/week02/1-Monday/homework/Extra_Problems_Answers.py
@@ -0,0 +1,15 @@
+import math
+
+
+# 1. Write a function called power which accepts a base and an exponent.
+# The function should return the power of the base to the exponent.
+
+
+# ----------------------------ANSWERS ------------------------------------
+# base = 4
+# exponent = 2
+# def power(base,exponent):
+#     product = base**exponent
+#     print(f"{base} to the power of {exponent} is {product}")
+
+# power(2,4)
