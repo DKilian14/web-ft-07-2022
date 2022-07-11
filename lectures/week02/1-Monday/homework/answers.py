fc086ad8d5d66999f492477dfc85485f50bd2dff
Dan Kilian <dankilian@yahoo.com>
2022-07-11 15:08:14 -0500

    completed week 2 homework

---
+++ lectures/week02/1-Monday/homework/answers.py
@@ -0,0 +1,89 @@
+#### 1. Find the largest number
+
+# Write a function `largest` that accepts a List of numbers as an argument.
+
+# It should return the largest number in the List.
+
+# ------------------------ANSWER --------------------------------------------
+# the_list = [15,2,3,4,17,22]
+
+# def largest(list_of_nums):
+    
+#     if len(list_of_nums):
+#         smallest_num = list_of_nums[0]
+#         for i in list_of_nums:
+#             if i < smallest_num:
+#                 smallest_num = i
+#     else: 
+#         smallest_num = 'nonexistant given the parameter.'
+#     print(f"Smallest number in the list is {smallest_num}")
+    
+# largest(the_list)
+
+
+#### 2. Find the largest number
+
+# Write a function `largest` that accepts a List of numbers as an argument.
+
+# It should return the largest number in the List.
+
+# -------------------------ANSWERS ----------------------------------------
+
+# the_list = [15,2,3,4,117,22]
+
+# def largest(list_of_nums):
+    
+#     if len(list_of_nums):
+#         largest_num = list_of_nums[0]
+#         for i in list_of_nums:
+#             if i > largest_num:
+#                 largest_num = i
+#     else: 
+#         largest_num = 'nonexistant given the parameter.'
+#     print(f"largest number in the list is {largest_num}")
+    
+# largest(the_list)
+
+#### 3. Find the shortest String
+
+# Write a function `shortest` that accepts a List of Strings as an argument.
+# It should return the shortest String in the List.
+
+# ------------------------------------ANSWERS ----------------------------------
+# the_list = ['hello', 'hi', 'hey','howdydoo','ohhaiMark']
+
+# def shortest(list_of_strings):
+#     if len(the_list)> 0:
+#         shortest_string = the_list[0]
+#         for i in list_of_strings:
+#             if len(i) < len(shortest_string):
+#                 shortest_string = i
+#     else: 
+#         shortest_string = 'nonexistant given the parameter.'        
+#     print(f"shortest string in the list is {shortest_string}")
+    
+
+# shortest(the_list)
+
+#### 4. Find the longest String
+
+# Write a function `longest` that accepts a List of Strings as an argument.
+
+# It should return the longest String in the List.
+
+# ------------------ANSWERS-----------------------------------------------
+
+the_list = ['hello', 'hi', 'hey','howdydoo','ohhaiMark']
+
+def shortest(list_of_strings):
+    if len(the_list)> 0:
+        shortest_string = the_list[0]
+        for i in list_of_strings:
+            if len(i) > len(shortest_string):
+                shortest_string = i
+    else: 
+        shortest_string = 'nonexistant given the parameter.'        
+    print(f"shortest string in the list is {shortest_string}")
+    
+
+shortest(the_list)
