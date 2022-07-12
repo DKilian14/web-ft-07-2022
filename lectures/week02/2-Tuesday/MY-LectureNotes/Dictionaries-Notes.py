95f2cce974c0da4843e18372ff968fe9ab00a926
Dan Kilian <dankilian@yahoo.com>
2022-07-12 13:09:06 -0500

    i dunno

--- lectures/week02/2-Tuesday/MY-LectureNotes/Dictionaries-Notes.py
+++ lectures/week02/2-Tuesday/MY-LectureNotes/Dictionaries-Notes.py
@@ -36,16 +36,6 @@ zodiac = {
 # ------------------------- del
 # del zodiac["Aries"] will remove "Aries" from the deictionary. 
 
-phonebook_dict = {
-    'Alice': '703-493-1834',
-    'Bob': '857-384-1234',
-    'Elizabeth': '484-584-2923'
-}
-
-print(phonebook_dict['Elizabeth'])
-phonebook_dict['Kareem'] = '938-489-1234'
-del phonebook_dict['Alice']
-phonebook_dict['Bob'] = '968-345-2345'
 
 
 # --------------------------- iterating
@@ -66,25 +56,3 @@ phonebook_dict['Bob'] = '968-345-2345'
     # Aquarius  :  The Thinker
     # Pisces  :  The Mystic
     
-ramit = {
-    'name': 'Ramit',
-    'email': 'ramit@gmail.com',
-    'interests': ['movies', 'tennis'],
-    'friends': [
-        {
-            'name': 'Jasmine',
-            'email': 'jasmine@yahoo.com',
-            'interests': ['photography', 'tennis']
-        },
-        {
-            'name': 'Jan',
-            'email': 'jan@hotmail.com',
-            'interests': ['movies', 'tv']
-        }
-    ]
-}
-
-print(ramit['email'])
-print(ramit['interests'][0])
-print(ramit['friends'][0]['email'])
-print(ramit['friends'][1]['interests'][1])
---
+++ lectures/week02/2-Tuesday/MY-LectureNotes/lab2.py
@@ -0,0 +1,12 @@
+import corona
+
+# Find the sum of all recovered cases
+
+
+# make it look like:
+# Name
+# Address (street, suite, city, zipcode)
+corona_data = corona.data
+
+for key in corona_data:
+    print(key)
