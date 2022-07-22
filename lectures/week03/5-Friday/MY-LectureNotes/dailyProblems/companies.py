9d09f9d7344d93b38698f8edb3b9525100d0d638
Dan Kilian <dankilian@yahoo.com>
2022-07-22 10:07:47 -0500

    pushing today's lecture

--- lectures/week03/5-Friday/MY-LectureNotes/dailyProblems/companies.py
+++ lectures/week03/5-Friday/MY-LectureNotes/dailyProblems/companies.py
@@ -1,55 +1,60 @@
+import imghdr
+import data 
+
+#? Loop through the company data and display logo image and company name on an html page. 
+#* to run the file:  python companies.py > index.html
+
+companies = data.data
+
+html = """
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Bootstrap Site</title>
+  <title>Companies</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-  <style>
-    .container-fluid .col-1{
-        border-left: 1px solid;
-        /* padding-left: 20px; */
-        width:100px;
-        
-    }
-
-    .container-fluid{
-        height:1000px;
-    }
-  </style>
-  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class="container">
+"""
+color = 'bg-warning'
 
 
-<div class="container-fluid bg-warning m-2">
-    <div class="container-fluid">
-        <div class="row">
-          <div class="col-4 bg-danger">
-            Our Company
-          </div>
-          <div class="col-1 offset-2 bg-danger">
-            Home
-          </div>
-          <div class="col-1 bg-warning">
-            Products
-          </div>
-          <div class="col-1 bg-danger">
-            Company
-          </div>
-          <div class="col-1">
-            Blog
-          </div>
-        </div>
+new_html = ''
+
+
+for i in companies:
+  if color == 'bg-warning':
+    color = 'bg-danger'
+  else: color = 'bg-warning'
+  
+  new_html += f"""
+    
+    <div class="row">
+      <div class="col-6 {color}">
+        {i['business_name']}
       </div>
+      <div class="col-6">
+        <img src="{i['logo']}" alt="business_logo">
+      </div>
+    </div>
+  """
+
+
+html+= new_html
+
+
+
+html += """
 </div>
-
-
-
-
-
-
 </body>
 </html>
+
+"""
+
+print(html)
--- lectures/week03/5-Friday/MY-LectureNotes/dailyProblems/problems.py
+++ lectures/week03/5-Friday/MY-LectureNotes/dailyProblems/problems.py
@@ -1,6 +1,17 @@
 # Return a new list with each element multiplied by 5
 
-let nums = [56, 34, 34, 1, 1, 1, 23, 12, 89, 89, 89, 70, 56] 
+nums = [56, 34, 34, 1, 1, 1, 23, 12, 89, 89, 89, 70, 56] 
 
+for i in range(len(nums)):
+    nums[i] = nums[i]*5
 
+print(nums)
 # Given a list [("name", "Elie"), ("job", "Instructor")], create a dictionary that looks like this {'job': 'Instructor', 'name': 'Elie'} (the order does not matter).
+
+info = [("name", "Elie"), ("job", "Instructor")]
+info_dict = {}
+
+for i in info:
+    info_dict[i[0]] = i[1]
+    
+print(info_dict)
